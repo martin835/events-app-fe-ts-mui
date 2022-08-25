ee9bbd3311168b9b08d46d32a6ee9f0900ef2cda
martin835 <64438132+martin835@users.noreply.github.com>
2022-08-25 13:54:54 +0200

    .

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,6 +1,17 @@
-import { Container, Grid, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Paper } from "@mui/material";
 import { IOrganizer } from "../interfaces/dbObjects";
 
@@ -34,6 +45,52 @@ const HomePage = () => {
             Prehľad divadiel na Slovensku
           </Typography>
         </Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Typography variant="h2" sx={{ fontSize: "2rem" }}>
+          Zoznam divadiel
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        >
+          {theaters?.map((theater) => (
+            <>
+              <ListItem alignItems="flex-start" key={theater._id}>
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src={`${theater.coverImg}`} />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={`${theater.name}`}
+                  secondary={
+                    <React.Fragment>
+                      {/* <Typography
+                        sx={{ display: "inline" }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                      >
+                        {theater.city}
+                      </Typography> */}
+                      <Button
+                        size="small"
+                        variant="outlined"
+                        sx={{ mt: "10px" }}
+                      >
+                        Viac
+                      </Button>
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+            </>
+          ))}
+        </List>
+      </Grid>
+      <Grid item xs={8} textAlign="center">
+        <Typography variant="h2" sx={{ fontSize: "2rem" }}>
+          Nove predstavenia
+        </Typography>
       </Grid>
     </Grid>
   );
